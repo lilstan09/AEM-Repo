0bfc147c860f2023c3444417a3d6d91b9fb64eeb
Stanley Jackson <lilstan09@gmail.com>
2021-07-22 10:38:35 -0400

    updates button javascript

--- javascript/javahome.js
+++ javascript/javahome.js
@@ -8,3 +8,24 @@ button.addEventListener("click", function () {
 })
 
 
+
+
+
+let fName = document.querySelector("#fName")
+function send() {
+    console.log(fullName.value);
+    console.log(tel.value);
+    console.log(mail.value);
+    console.log(comments.value);
+}
+
+function phonenumber(inputtxt) {
+    let phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    if ((inputtxt.value.match(phoneno))) {
+        return true;
+    }
+    else {
+        alert("Not a valid Phone Number");
+        return false;
+    }
+}
