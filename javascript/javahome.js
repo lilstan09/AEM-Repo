b3c0ca5d6fc76015ccf754078687a7d5e2ad0d2f
Stanley Jackson <lilstan09@gmail.com>
2021-07-22 11:24:56 -0400

    added validation for phone and email.

--- javascript/javahome.js
+++ javascript/javahome.js
@@ -1,3 +1,29 @@
+
+function phonenumber(inputtxt) {
+    let phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    if (inputtxt.value.match(phoneno)) {
+        return true;
+    }
+    else {
+        alert("Not a valid 10 digit Phone Number");
+        return false;
+    }
+}
+
+
+function ValidateEmail(inputText) {
+    let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (inputText.value.match(mailformat)) {
+        return true;
+    }
+    else {
+        alert("You have entered an invalid email address!");
+        document.form1.email.focus();
+        return false;
+    }
+}
+
+
 const button = document.getElementById('btn');
 const content = document.getElementById('aboutButton')
 
@@ -7,25 +33,10 @@ button.addEventListener("click", function () {
 
 })
 
-
-
-
-
-let fName = document.querySelector("#fName")
+let form = document.querySelector("formData")
 function send() {
     console.log(fullName.value);
     console.log(tel.value);
     console.log(mail.value);
     console.log(comments.value);
-}
-
-function phonenumber(inputtxt) {
-    let phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-    if ((inputtxt.value.match(phoneno))) {
-        return true;
-    }
-    else {
-        alert("Not a valid Phone Number");
-        return false;
-    }
 }
